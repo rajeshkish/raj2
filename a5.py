19cd5ff686cfa36cc3399c5c4e68faf85b6c43fa
rajeshkish <52270436+rajeshkish@users.noreply.github.com>
2019-07-16 12:09:06 +0530

    Update a5.py

--- a5.py
+++ a5.py
@@ -1,11 +1,11 @@
-year = 2004
+year = 2008
 
 # To get year (integer input) from the user
 # year = int(input("Enter a year: "))
 
-if (year % 4) == 0:
-   if (year % 100) == 0:
-       if (year % 400) == 0:
+if (year % 6) == 0:
+   if (year % 200) == 0:
+       if (year % 600) == 0:
            print("{0} is a leap year".format(year))
        else:
            print("{0} is not a leap year".format(year))
